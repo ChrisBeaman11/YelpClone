24f935656d8c03bb62c5fd8c734f8c00760d668b
Christopher Beaman <ChrisBeaman97@gmail.com>
2023-07-12 04:25:09 -0400

    backend setup phase 01

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
@@ -13,5 +17,5 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
-  
+
 module.exports = router;
