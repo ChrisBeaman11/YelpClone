208057cad088b4ed988b3826b13e5eeaa07c4ccc
Christopher Beaman <ChrisBeaman97@gmail.com>
2023-07-17 11:25:03 -0400

    route done

--- backend/routes/api/locations.js
+++ backend/routes/api/locations.js
@@ -3,5 +3,39 @@ const { setTokenCookie, requireAuth } = require("../../utils/auth");
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+    let arr = [];
+
+    const locations = await Location.findAll();
+    for (let location of locations) {
+      const numOfReviews = await Review.count({
+        where: {
+          spotId: spot.id,
+        },
+      });
+      const totalStars = await Review.findAll({
+        attributes: [[sequelize.fn("sum", sequelize.col("stars")), "total"]],
+        where: {
+          locationId: location.id,
+        },
+      });
+      let avgStars = totalStars[0].dataValues.total / numOfReviews;
+      let pojo = location.toJSON();
+      location.avgRating = avgStars;
+      const previewImg = await LocationImage.findAll({
+        where: {
+          locationId: location.id,
+        },
+      });
+      if (previewImg.length) {
+        pojo.previewImage = previewImg[0].dataValues.url;
+      }
+      if (!previewImg.length) {
+        pojo.previewImage = null;
+      }
+      arr.push(pojo);
+    }
+    return res.json({ Locations: arr });
+  });
 
 module.exports = router;
